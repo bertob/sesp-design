ff2c95b9f640ddfead87e081accf53e1dbfbda54
Tobias Bernard <bertob@zenbo.ok>
2013-11-02 17:39:50 +0100

    / ‘script.js’

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("").event(function() {
+    $(".expirationrow").event(function() {
         $(thingToAffect).effect();
         $('#div a').after('<p>html element to add</p>');
 
